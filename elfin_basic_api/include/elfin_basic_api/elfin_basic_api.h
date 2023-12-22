b6e9221d764bb0b67e626c18d485485f564b0281
indy <bleubirb@users.noreply.github.com>
2023-12-21 20:48:12 -0800

    understanding for dev

--- elfin_basic_api/include/elfin_basic_api/elfin_basic_api.h
+++ elfin_basic_api/include/elfin_basic_api/elfin_basic_api.h
@@ -56,23 +56,59 @@ class ElfinBasicAPI
 {
 public:
     ElfinBasicAPI(moveit::planning_interface::MoveGroupInterface *group, std::string action_name, planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor);
+    // moveit is a pointer to a MoveGroupInterface object, which is used to control the robot
+    // planning_interface is a pointer to a PlanningSceneInterface object, which is used to add or remove objects in the planning scene
+    // action_name is the name of the action server, which is used to control the robot
+    // planning_scene_monitor is a pointer to a PlanningSceneMonitor object, which is used to get the current state of the robot
     ~ElfinBasicAPI();
+    // destructor
 
     void dynamicReconfigureCallback(ElfinBasicAPIDynamicReconfigureConfig &config, uint32_t level);
+    // callback function for dynamic reconfigure, which is used to change the velocity scaling of the robot
+    // config is the configuration of the dynamic reconfigure
+    // level is the level of the dynamic reconfigure
+    // callback means that this function will be called when the velocity scaling of the robot is changed
 
     void setVelocityScaling(double data);
+    // set the velocity scaling of the robot, the value of which is in the range of [0, 1], and the default value is 0.1, which means the robot moves at 10% of its maximum velocity
 
     bool setVelocityScaling_cb(elfin_robot_msgs::SetFloat64::Request &req, elfin_robot_msgs::SetFloat64::Response &resp);
+    // callback function for the service server, which is used to change the velocity scaling of the robot
+    // req is the request of the service server
+    // resp is the response of the service server
+
     bool updateVelocityScaling_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &resp);
+    // callback function for the service server, which is used to update the velocity scaling of the robot
+    // req is the request of the service server
+    // resp is the response of the service server
 
     bool setRefLink_cb(elfin_robot_msgs::SetString::Request &req, elfin_robot_msgs::SetString::Response &resp);
+    // callback function for the service server, which is used to set the reference link of the robot, reference link is the link that the robot moves relative to
+    // example: if the reference link is "base_link" and the end link is "link_6", the robot will move relative to the base_link and move the link_6 to the target position
+    
     bool setEndLink_cb(elfin_robot_msgs::SetString::Request &req, elfin_robot_msgs::SetString::Response &resp);
+    // callback function for the service server, which is used to set the end link of the robot, end link is the link that the robot moves to
+    // example: if the end link is "base_link", the robot will move to the position of the base_link
+
     bool enableRobot_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &resp);
+    // callback function for the service server, which is used to enable the robot
+    // std_srvs is a package that contains the service server and the service client
+
     bool disableRobot_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &resp);
+    // callback function for the service server, which is used to disable the robot
 
     bool stopActCtrlrs(std_srvs::SetBool::Response &resp);
+    // stop the action controllers of the robot
+  
     bool startElfinCtrlr(std_srvs::SetBool::Response &resp);
+    // start the elfin controller of the robot
+
     void posePubTimer_cb(const ros::TimerEvent& evt);
+    // callback function for the timer, which is used to publish the pose of the robot
+    // publish for ROS means that the data is sent to the topic, and the data can be received by the subscriber of the topic
+    // the topic is a channel that can be used to send and receive data
+    // example: the desired position of the robot is sent to the topic, and the desired position of the robot is received from the topic
+
 private:
     moveit::planning_interface::MoveGroupInterface *group_;
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
@@ -116,11 +152,11 @@ private:
     ros::Publisher ref_link_name_publisher_;
     ros::Publisher end_link_name_publisher_;
 
-    tf::TransformListener tf_listener_;
-    ros::Timer pub_pose_timer;
-    ros::Publisher elfin_pose;
-    tf::StampedTransform poseTransForm;
-    tf::Quaternion RQ;
+    tf::TransformListener tf_listener_; 
+    ros::Timer pub_pose_timer; 
+    ros::Publisher elfin_pose; // used to publish the pose of the robot
+    tf::StampedTransform poseTransForm; // used to get the pose of the robot
+    tf::Quaternion RQ; // used to get the rotation of the robot
 };
 
 }
